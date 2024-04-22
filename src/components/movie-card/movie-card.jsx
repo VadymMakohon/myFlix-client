6bed5f5eb36525364020a3293ec1dac6048e8ed4
VadymMakohon <errorks7@gmail.com>
2024-04-22 17:24:19 +0200

    updated the movie card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
@@ -8,4 +10,15 @@ export const MovieCard = ({ movie, onMovieClick }) => {
             {movie.title}
         </div>
     );
+};
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        genre: PropTypes.string,
+        director: PropTypes.string,
+        featured: PropTypes.bool
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 };
