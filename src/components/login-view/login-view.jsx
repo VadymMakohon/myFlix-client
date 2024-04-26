b2bff6162f1b2bd707905fa199603a7adce082ce
VadymMakohon <errorks7@gmail.com>
2024-04-26 17:35:02 +0200

    modified login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -11,6 +11,7 @@ export const LoginView = ({ onLoggedIn }) => {
             Password: password
         };
 
+
         fetch("https://myflix-2024-e9df13718d8a.herokuapp.com/login", {
             method: "POST",
             headers: {
@@ -35,20 +36,20 @@ export const LoginView = ({ onLoggedIn }) => {
     }
     return (
         <form onSubmit={handleSubmit}>
-            <label>Username:</label>
-            <input type="text"
-                minLength={5}
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required />
-            <br />
-            <label>Password:</label>
-            <input type="password"
-                minLength={8}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required />
-            <br />
+            <label>
+                Username:
+                <input type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required />
+            </label>
+            <label>
+                Password:
+                <input type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required />
+            </label>
             <button type="submit">Submit</button>
         </form>
     );
