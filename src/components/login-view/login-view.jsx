818591ee73475ddcaebbbab2510dec17a9776fbf
VadymMakohon <errorks7@gmail.com>
2024-04-26 16:47:24 +0200

    Created login-view and signup-view and modified main-view file as well as added logout button to it

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useState } from "react";
+export const LoginView = ({ onLoggedIn }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            Username: username,
+            Password: password
+        };
+
+
+        fetch("https://myflix-2024-e9df13718d8a.herokuapp.com/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then((response) => response.json())
+            .then((data) => {
+                console.log("Login response: ", data);
+                if (data.user) {
+                    localStorage.setItem("user", JSON.stringify(data.user));
+                    localStorage.setItem("token", data.token);
+                    onLoggedIn(data.user, data.token);
+                } else {
+                    alert("No such user");
+                }
+            })
+            .catch((e) => {
+                console.error("Login error: ", e);
+                alert("Something went wrong");
+            });
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required />
+            </label>
+            <label>
+                Password:
+                <input type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
     const [movies, setMovies] = useState([]);
-
     const [selectedMovie, setSelectedMovie] = useState(null);
+    const [user, setUser] = useState(storedUser ? storedUser : null);
+    const [token, setToken] = useState(storedToken ? storedToken : null);
+
+    if (!token) {
+        return;
+    }
 
     useEffect(() => {
-        fetch("https://myflix-2024-e9df13718d8a.herokuapp.com/movies")
+        fetch("https://myflix-2024-e9df13718d8a.herokuapp.com/movies", {
+            headers: { Authorization: `Bearer ${token}` }
+        })
             .then((response) => response.json())
             .then((data) => {
+                console.log("Movies data: ", movie);
                 const moviesFromApi = data.map((movie) => {
                     return {
                         id: movie._id,
@@ -25,9 +37,20 @@ export const MainView = () => {
                 setMovies(moviesFromApi);
 
             });
-    }, []);
+    }, [token]);
+
+    if (!user) {
+        return (
+            <>
+                <LoginView onLoggedIn={(user, token) => { setUser(user); setToken(token) }} />
+                or
+                <SignupView />
+            </>
+        )
+    }
 
     if (selectedMovie) {
+        // console.log(selectedMovie);
         return (
             <MovieView movie={selectedMovie}
                 onBackClick={() => setSelectedMovie(null)} />
@@ -47,6 +70,7 @@ export const MainView = () => {
                     }}
                 />
             ))}
+            <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
         </div>
     );
 };
