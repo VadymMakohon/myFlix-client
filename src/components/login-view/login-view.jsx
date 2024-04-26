7a106e50edbc3b0b47ec7c1c9d6292b184f0d66e
VadymMakohon <errorks7@gmail.com>
2024-04-26 17:40:12 +0200

    changed onSubmit form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -35,22 +35,30 @@ export const LoginView = ({ onLoggedIn }) => {
             });
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input type="text"
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
+                    type="text"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
-                    required />
-            </label>
-            <label>
-                Password:
-                <input type="password"
+                    required
+                    minLength="3"
+                />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
+                    type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    required />
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+                    required
+                />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     );
 };
