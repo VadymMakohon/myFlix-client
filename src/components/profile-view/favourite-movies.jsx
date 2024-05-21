1e23cf2fb371555ef41007c320db85e840545596
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-05-22 00:00:42 +0200

    Update favourite-movies.jsx
    modified the fav movies

--- src/components/profile-view/favourite-movies.jsx
+++ src/components/profile-view/favourite-movies.jsx
@@ -6,26 +6,28 @@ import { Link } from "react-router-dom";
 import { MovieCard } from "../movie-card/movie-card";
 import "./favourite-movies.scss"
 
-export const FavouriteMovies = ({ user, favouriteMovies, removeFromFavorites }) => {
+export const FavouriteMovies = ({ user, favouriteMovies }) => {
     return (
-        <Col className="mb-5">
-            <h3 className="title">Favorite movies</h3>
+        <Row>
+            <Col className="mb-5">
+                <h3>Favorite movies</h3>
+            </Col>
             <Row>
-                {favouriteMovies.map((movie) => (
-                    <Col key={movie._id} md={6}>
-                        <Link to={`/movies/${movie._id}`} />
-                        <MovieCard
-                            key={movie._id}
-                            isFavorite={user.FavouriteMovies.includes(movie.title)}
-                            movie={movie}
-                            removeFromFavorites={removeFromFavorites}
-                        />
-                    </Col>
-                ))}
+                {favouriteMovies.map((movie) => {
+                    return (
+                        <Col className="mb-5" key={movie.id} md={4}>
+                            <Link to={`/movies/${movie.Title}`} />
+                            <MovieCard
+                                movie={movie}
+                                isFavourite={user.favouriteMovies.includes(movie.Title)}
+                            />
+                        </Col>
+                    );
+                })}
             </Row>
-        </Col>
-    );
-};
+        </Row>
+    )
+}
 
 FavouriteMovies.propTypes = {
     favouriteMovies: PropTypes.array.isRequired,
