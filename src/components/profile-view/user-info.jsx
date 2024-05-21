32ff5077e6da4cca8548710d1d0dd8970c23f09c
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-05-22 00:04:01 +0200

    Update user-info.jsx
    added propTypes to user

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Card } from 'react-bootstrap';
 
 export const UserInfo = ({ email, name }) => {
     return (
         <div>
-            <Card.Text>username: {name}</Card.Text>
-            <Card.Text>email: {email}</Card.Text>
+            <Card.Text>Username: {name}</Card.Text>
+            <Card.Text>Email: {email}</Card.Text>
         </div>
     );
+};
+UserInfo.propTypes = {
+    email: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
 };
